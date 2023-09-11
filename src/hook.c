0f5e593757da9b91cb84079003ab42f3b48af3f2
Meri Eskelinen <meskelin@c1r5p19.hive.fi>
2023-09-11 14:24:54 +0300

    Fix hook function signature

--- src/hook.c
+++ src/hook.c
@@ -1,14 +1,15 @@
+
 #include "../headers/cubed.h"
 
-int key_hook(t_mlx **mlx, int keycode)
+int key_hook(int keycode, t_mlx **mlx)
 {
-    if (keycode == ESC)
-        exit_success(mlx);
-    return (0);
+	if (keycode == ESC)
+		exit_success(mlx);
+	return (0);
 }
 
 int on_destroy(t_mlx **mlx)
 {
-    exit_success(mlx);
-    return(0);
+	exit_success(mlx);
+	return(0);
 }
