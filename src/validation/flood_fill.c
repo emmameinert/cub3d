ac9530d65af256203a87e11c8ca77a4e36b490fd
Emma Meinert <emmameinert@Emmas-Air-3.fritz.box>
2023-09-12 15:10:20 +0200

    checks for start point and stops at spaces

--- src/validation/flood_fill.c
+++ src/validation/flood_fill.c
@@ -1,5 +1,14 @@
 #include "../../headers/cubed.h"
 
+static  void    validate_start_point(t_textures **texture)
+{
+    if ((*texture)->x_start == 0 || (*texture)->x_start == (*texture)->m_width)
+        ft_put_error_exit("Start point out of maze");
+    if ((*texture)->y_start == 0 || (*texture)->y_start == (*texture)->m_height)
+        ft_put_error_exit("Start point out of maze");
+    //need to check for spaces around
+}
+
 void  flood_fill(t_textures **texture)
 {
     int i;
@@ -7,9 +16,10 @@ void  flood_fill(t_textures **texture)
     i = (*texture)->x_start;
     j = (*texture)->y_start;
     (*texture)->map[j][i].ch = '0';
+    validate_start_point(texture);
     while (j < (*texture)->m_height)
     {
-        while (i < (*texture)->m_width)
+        while (i < (*texture)->m_width && (*texture)->map[j][i].ch != ' ')
         {
             if (i >= 0 && i < (*texture)->m_width && j >= 0 && j <  (*texture)->m_height && (*texture)->map[j][i].ch == '1')
                 (*texture)->map[j][i].ch = '2';
@@ -19,10 +29,10 @@ void  flood_fill(t_textures **texture)
         j++;
     }
     j = 0;
-    while (j < (*texture)->y_start)
+    while (j < (*texture)->y_start && (*texture)->map[j][i].ch != ' ')
     {
         i = 0;
-        while (i < (*texture)->m_width)
+        while (i < (*texture)->m_width && (*texture)->map[j][i].ch != ' ')
         {
             if (i >= 0 && i < (*texture)->m_width && j >= 0 && j <  (*texture)->m_height && (*texture)->map[j][i].ch == '1')
                 (*texture)->map[j][i].ch = '2';
@@ -33,7 +43,7 @@ void  flood_fill(t_textures **texture)
     if (j == (*texture)->y_start)
     {
         i = 0;
-        while (i < (*texture)->x_start)
+        while (i < (*texture)->x_start && (*texture)->map[j][i].ch != ' ')
         {
             if (i >= 0 && i < (*texture)->m_width && j >= 0 && j <  (*texture)->m_height && (*texture)->map[j][i].ch == '1')
                 (*texture)->map[j][i].ch = '2';
