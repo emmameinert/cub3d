0fcb0a4f836760300009e37f45888e7aca71f475
Meri Eskelinen <merituulie@hotmail.fi>
2023-09-14 20:52:28 +0300

    Add check for inaccessible area

--- src/validation/flood_fill.c
+++ src/validation/flood_fill.c
@@ -14,7 +14,6 @@ void	flood_fill(t_info **info, int y, int x)
 	t_coord *temp;
 
 	temp = &(*info)->map[y][x];
-	printf("evaluating '%c' on [%d,%d]\n", temp->ch, temp->y, temp->x);
 	if (temp->ch == ' ')
 		ft_put_error_exit("Invalid map");
 	else if (temp->checked)
@@ -29,56 +28,3 @@ void	flood_fill(t_info **info, int y, int x)
 	flood_fill(info, temp->y + 1, temp->x);
 	flood_fill(info, temp->y, temp->x + 1);
 }
-
-//static  void    validate_start_point(t_info **info)
-//{
-//    if ((*info)->begin->x == 0 || (*info)->begin->x == (*info)->m_width)
-//        ft_put_error_exit("Start point out of maze");
-//    if ((*info)->begin->y == 0 || (*info)->begin->y == (*info)->m_height)
-//        ft_put_error_exit("Start point out of maze");
-//    //need to check for spaces around
-//}
-
-//void  flood_fill(t_info **info)
-//{
-//    int i;
-//    int j;
-//    i = (*info)->begin->x;
-//    j = (*info)->begin->y;
-//    (*info)->map[j][i].ch = '0';
-//    validate_start_point(info);
-//    while (j < (*info)->m_height)
-//    {
-//        while (i < (*info)->m_width && (*info)->map[j][i].ch != ' ')
-//        {
-//            if (i >= 0 && i < (*info)->m_width && j >= 0 && j <  (*info)->m_height && (*info)->map[j][i].ch == '1')
-//                (*info)->map[j][i].ch = '2';
-//            i++;
-//        }
-//        i = 0;
-//        j++;
-//    }
-//    j = 0;
-//    while (j < (*info)->begin->y && (*info)->map[j][i].ch != ' ')
-//    {
-//        i = 0;
-//        while (i < (*info)->m_width && (*info)->map[j][i].ch != ' ')
-//        {
-//            if (i >= 0 && i < (*info)->m_width && j >= 0 && j <  (*info)->m_height && (*info)->map[j][i].ch == '1')
-//                (*info)->map[j][i].ch = '2';
-//            i++;
-//        }
-//        j++;
-//    }
-//    if (j == (*info)->begin->y)
-//    {
-//        i = 0;
-//        while (i < (*info)->begin->x && (*info)->map[j][i].ch != ' ')
-//        {
-//            if (i >= 0 && i < (*info)->m_width && j >= 0 && j <  (*info)->m_height && (*info)->map[j][i].ch == '1')
-//                (*info)->map[j][i].ch = '2';
-//            i++;
-//        }
-//    }
-//}
-
--- src/validation/map_validation.c
+++ src/validation/map_validation.c
@@ -31,11 +31,30 @@ static void	print_map(t_info **info)
 		i++;
 	}
 }
+
+static void	validate_inaccessables(t_info **info)
+{
+	t_coord	*temp;
+
+	(*info)->y_index = 0;
+	(*info)->x_index = 0;
+	while ((*info)->y_index < (*info)->m_height)
+	{
+		while ((*info)->x_index < (*info)->m_width)
+		{
+			temp = &(*info)->map[(*info)->y_index][(*info)->x_index++];
+			if (!temp->checked && temp->ch == '0')
+				ft_put_error_exit("Invalid map");
+		}
+		(*info)->x_index = 0;
+		(*info)->y_index++;
+	}
+}
+
 void	validate_map(t_node **map, t_info **info)
 {
 	if (!map || !*info)
 		ft_put_error_exit("No map found");
 	flood_fill(info, (*info)->player->y, (*info)->player->x);
-	//printf("\n\n");
-	//print_map(info);
+	validate_inaccessables(info);
 }
