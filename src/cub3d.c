5ffa2636bc92aa5518d73a8f7e6f6e343e2fbaf1
Meri Eskelinen <meskelin@c1r5p19.hive.fi>
2023-09-18 13:05:10 +0300

    Remove one dot from texture path

--- src/cub3d.c
+++ src/cub3d.c
@@ -18,7 +18,7 @@ void run_program(char **argv)
     // validate_parse_file(fd, &info);
     mlx = init_mlx_wind();
     mlx->img = init_img(&mlx);
-    mlx->img->img = mlx_xpm_file_to_image(mlx->mlx, "../textures/grass.xpm", &width, &height);
+    mlx->img->img = mlx_xpm_file_to_image(mlx->mlx, "./textures/grass.xpm", &width, &height);
     mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img->img, 0, 0);
     mlx_hook(mlx->window, ON_KEYDOWN, 1, key_hook, &mlx);
 	mlx_hook(mlx->window, ON_DESTROY, 1, on_destroy, &mlx);
