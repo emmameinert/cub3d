29cfe281c9bd6b41b82936702a95795c95ac8297
Emma Meinert <emmameinert@Emmas-MacBook-Air-3.local>
2023-09-26 10:49:58 +0300

    fixed angles and start position

--- src/parsing/player_parsing.c
+++ src/parsing/player_parsing.c
@@ -3,16 +3,16 @@
 
 void	parse_player(t_player **player)
 {
-	(*player)->x *= MINI_SIZE + PLR_SIZE;
-	(*player)->y *= MINI_SIZE + PLR_SIZE;
+	(*player)->x = (*player)->x * MINI_SIZE + PLR_SIZE;
+	(*player)->y = (*player)->y * MINI_SIZE + PLR_SIZE;
 	if ((*player)->dir == 'E')
 		(*player)->angle = 0;
 	if ((*player)->dir == 'S')
-		(*player)->angle = 90;
+		(*player)->angle = 270;
 	if ((*player)->dir == 'W')
 		(*player)->angle = 180;
 	if ((*player)->dir == 'N')
-		(*player)->angle = 270;
+		(*player)->angle = 90;
 	(*player)->dir_x = cos(ft_dtorad((*player)->angle));
 	(*player)->dir_y = -sin(ft_dtorad((*player)->angle));
 }
