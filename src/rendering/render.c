08abd84107f311da5f1b208764ef8b844773f1a7
Meri Eskelinen <meskelin@c1r5p19.hive.fi>
2023-10-03 12:17:52 +0300

    Add fix for the render

--- src/rendering/render.c
+++ src/rendering/render.c
@@ -7,5 +7,6 @@ void	render(t_info **info)
 	draw_background(info, WIN_HEIGHT, WIN_WIDTH);
 	draw_minimap(info);
 	draw_player(info);
+	// Add texture file protection to check that texture files are accessable
 	mlx_put_image_to_window((*info)->mlx->mlx, (*info)->mlx->window, (*info)->mlx->img->img, 0, 0);
 }
