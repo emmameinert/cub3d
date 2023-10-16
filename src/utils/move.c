e975f5be43a48938c541ef563b965e32e2457c5c
meri <merituulie@hotmail.fi>
2023-10-16 17:39:16 +0300

    Fix player movement collision hit

--- src/utils/move.c
+++ src/utils/move.c
@@ -19,11 +19,14 @@ void	move_front_back(int sign_x, int sign_y, t_info **info)
 	double nextY;
 
 	nextX = (*info)->player->x + (sign_x * ((*info)->player->dir_x * (PLR_STEP * 5)));
+	if ((*info)->map[(int)(*info)->player->y / GRID_SIZE]
+		&& (*info)->map[(int)(*info)->player->y
+		/ GRID_SIZE][(int)nextX / GRID_SIZE].ch != '1')
+		(*info)->player->x = (*info)->player->x + (sign_x * (*info)->player->dir_x * PLR_STEP);
 	nextY = (*info)->player->y + (sign_y * ((*info)->player->dir_y * (PLR_STEP * 5)));
-	if ((*info)->map[(int)nextY / GRID_SIZE] && (*info)->map[(int)nextY / GRID_SIZE][(int)nextX / GRID_SIZE].ch == '1')
-		return ;
-	(*info)->player->x = (*info)->player->x + (sign_x * (*info)->player->dir_x * PLR_STEP);
-	(*info)->player->y = (*info)->player->y + (sign_y * (*info)->player->dir_y * PLR_STEP);
+	if ((*info)->map[(int)nextY / GRID_SIZE]
+		&& (*info)->map[(int)nextY / GRID_SIZE][(int)nextX / GRID_SIZE].ch != '1')
+		(*info)->player->y = (*info)->player->y + (sign_y * (*info)->player->dir_y * PLR_STEP);
 }
 
 void	move_side(int sign_x, int sign_y, t_info **info)
