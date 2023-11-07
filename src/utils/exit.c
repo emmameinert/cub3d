efef958a0734b33df10e441f599768e34eee1368
meri <merituulie@hotmail.fi>
2023-11-07 11:52:00 +0200

    Clean up code

--- src/utils/exit.c
+++ src/utils/exit.c
@@ -6,46 +6,43 @@
 /*   By: meskelin <meskelin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:50:02 by meskelin          #+#    #+#             */
-/*   Updated: 2023/11/07 11:46:41 by meskelin         ###   ########.fr       */
+/*   Updated: 2023/11/07 11:51:43 by meskelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/cubed.h"
 
-static void	free_matrix(t_texture **tex)
+static void	free_tex(t_info **info, t_texture **tex)
 {
 	int	i;
 
-	i = -1;
-	while (++i < (*tex)->height)
-		free((*tex)->matrix[i]);
-	free((*tex)->matrix);
+	if (!*tex)
+		return ;
+	if ((*tex)->filename)
+		free((*tex)->filename);
+	if ((*tex)->matrix)
+	{
+		i = -1;
+		while (++i < (*tex)->height)
+			free((*tex)->matrix[i]);
+		free((*tex)->matrix);
+	}
+	if ((*tex)->img)
+	{
+		mlx_destroy_image((*info)->mlx->mlx, (*tex)->img->img);
+		free((*tex)->img);
+	}
+	free(*tex);
 }
 
 static void	free_mlx(t_info **info)
 {
 	mlx_destroy_image((*info)->mlx->mlx, (*info)->mlx->img->img);
 	free((*info)->mlx->img);
-	free((*info)->no->filename);
-	free_matrix(&(*info)->no);
-	mlx_destroy_image((*info)->mlx->mlx, (*info)->no->img->img);
-	free((*info)->no->img);
-	free((*info)->no);
-	free((*info)->so->filename);
-	free_matrix(&(*info)->so);
-	mlx_destroy_image((*info)->mlx->mlx, (*info)->so->img->img);
-	free((*info)->so->img);
-	free((*info)->so);
-	free((*info)->ea->filename);
-	free_matrix(&(*info)->ea);
-	mlx_destroy_image((*info)->mlx->mlx, (*info)->ea->img->img);
-	free((*info)->ea->img);
-	free((*info)->ea);
-	free((*info)->we->filename);
-	free_matrix(&(*info)->we);
-	mlx_destroy_image((*info)->mlx->mlx, (*info)->we->img->img);
-	free((*info)->we->img);
-	free((*info)->we);
+	free_tex(info, &(*info)->no);
+	free_tex(info, &(*info)->so);
+	free_tex(info, &(*info)->ea);
+	free_tex(info, &(*info)->we);
 	mlx_destroy_window((*info)->mlx->mlx, (*info)->mlx->window);
 }
 
